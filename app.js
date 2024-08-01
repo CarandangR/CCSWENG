d56a159f04471c9791d5755a6c5d45cf60318e56
CarandangR <133246912+CarandangR@users.noreply.github.com>
2024-08-02 01:51:42 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+dotenv.config();
+
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+const PORT = process.env.PORT;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then (()=>{
+    console.log("DB Connnect successfully");
+    app.listen(PORT, () =>{
+        console.log("Server is Running on Port "+ PORT);
+    });
+}).catch((error)=> console.log(error))
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'CSSWENG Home page.html'));
+});
+
+app.get("/Encoder", function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'CSSWENG Encoder.html'));
+});
+
+app.get("/Heat_Index", function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'CSSWENG Heat.html'));
+});
